c6d78eb585e5519471e39dd4cce0267edf547abf
Jack Dempsey <jack.dempsey@gmail.com>
2010-09-14 13:06:20 -0400

    bind to local aruba

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "http://rubygems.org"
 
 gem "cucumber", "0.8.5"
+gem "aruba", :path => '/Users/jack/git/aruba'
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,9 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
 require 'writeme'
 
+require "rubygems"
+require "bundler/setup" #setup makes the :path to Aruba work
+require 'aruba'
 require 'test/unit/assertions'
 
 World(Test::Unit::Assertions)
