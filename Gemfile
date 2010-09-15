8833b0837796da792d10d96e628a6751336e8d36
Jack Dempsey <jack.dempsey@gmail.com>
2010-09-15 16:36:37 -0400

    be smart about including ruby-debug support

--- Gemfile
+++ Gemfile
@@ -2,4 +2,10 @@ source "http://rubygems.org"
 
 gem "cucumber", "0.8.5"
 gem "aruba", :path => '/Users/jack/git/aruba'
+if RUBY_VERSION < '1.9'
+  gem "ruby-debug", "~> 0.10.3"
+else
+  gem 'ruby-debug19', '0.11.6'
+end
+
 gem "thor"
--- features/support/env.rb
+++ features/support/env.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
 require 'writeme'
 
 require "rubygems"
+require "ruby-debug"
 require "bundler/setup" #setup makes the :path to Aruba work
 require 'aruba'
 require 'test/unit/assertions'
