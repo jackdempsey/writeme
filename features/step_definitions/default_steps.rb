e013cfca4d7fe9c62c4a87be7b2e56467f3f85aa
Jack Dempsey <jack.dempsey@gmail.com>
2010-09-16 16:38:03 -0400

    combine steps

--- features/step_definitions/default_steps.rb
+++
@@ -1,3 +0,0 @@
-Given /^I have a config file with:$/ do |data|
-  Given %{a file named "#{Writeme.defaults_filename}" with:}, data
-end
--- features/step_definitions/writeme_steps.rb
+++ features/step_definitions/writeme_steps.rb
@@ -1,3 +1,15 @@
+Given /^I have a config file with:$/ do |data|
+  Given %{a file named "#{Writeme.defaults_filename}" with:}, data
+end
+
+When /^I generate a README(?: without specifying a creator)?$/ do
+  When %{I run writeme}
+end
+
+Then /^the README file should contain "([^"]*)"$/ do |text|
+  Then %{the file "#{Writeme.output_filename}" should contain "#{text}"}
+end
+
 When /^I run writeme with no arguments$/ do
   When %{I run writeme with ""}
 end
